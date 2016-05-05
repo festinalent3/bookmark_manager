b4e4e9a08c8850175f657d4d5b5568e0cb2eaa27
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-05-05 18:33:14 +0200

    edited data_mapper_setup

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -7,4 +7,3 @@ require_relative 'models/link'
 #DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
-DataMapper.auto_upgrade!
